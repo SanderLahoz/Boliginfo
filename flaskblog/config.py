9ad116988ebf32102e11e917a3d1ac4ca2c6a6b7
Sander <73191374+SanderLahoz@users.noreply.github.com>
2024-03-11 19:38:41 +0000

    Adding some changes

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,12 +1,15 @@
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 class Config:
-	SECRET_KEY = os.environ.get("SOP_SECRET_KEY")
-	SQLALCHEMY_DATABASE_URI = os.environ.get("SOP_SQL_URI")
+	SECRET_KEY = os.getenv("SOP_SECRET_KEY")
+	SQLALCHEMY_DATABASE_URI = os.getenv("SOP_SQL_URI")
 	MAIL_SERVER = "smtp.gmail.com"
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-	MAIL_PASSWORD = os.environ.get("SOP_APP_PASS")
-	STRIPE_PUBLIC = os.environ.get("STRIPE_PUBLIC_TEST")
-	STRIPE_SECRET = os.environ.get("STRIPE_SECRET_TEST")
+	MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+	MAIL_PASSWORD = os.getenv("SOP_APP_PASS")
+	STRIPE_PUBLIC = os.getenv("STRIPE_PUBLIC_TEST")
+	STRIPE_SECRET = os.getenv("STRIPE_SECRET_TEST")
--- flaskblog/contacts/utils.py
+++ flaskblog/contacts/utils.py
@@ -1,12 +1,15 @@
 from flaskblog import mail
 from flask_mail import Message
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 def send_mail(name, email, subject, content):
 
 	msg = Message(subject, 
 		sender="noreply@contact-boliginfo.com",
-		recipients=[os.environ.get("MAIL_USERNAME")])
+		recipients=[os.getenv("MAIL_USERNAME")])
 	msg.body = f'''A user has submitted an invoice via the contact form
 Name of sender: {name}
 Email of sender: {email}
